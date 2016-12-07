7790320b51c1ca4cc436be5cc442f33275acc4d8
rex.he <rex@rex-he.net>
2016-12-07 11:33:36 +1100

    Update PSL_Demo.ps1

--- Module/Demo/PSL_Demo.ps1
+++ Module/Demo/PSL_Demo.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module C:\Repos\PowerShellLoggingModule\Module\PowerShellLogging
+﻿Import-Module ..\PowerShellLogging
 
 $LogFile = Enable-LogFile #-Path C:\Log\ScriptName_Log_$(Get-Date -Format "yyyy-MM-dd_HHmmss").txt
 
